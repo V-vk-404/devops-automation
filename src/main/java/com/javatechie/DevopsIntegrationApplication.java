66c6488ee5b09309b2ab7e67bdec92985fc4bc63
Vikash Soni <78948052+V-vk-404@users.noreply.github.com>
2023-10-22 17:46:10 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "<h1> Welcome To MavenProject </h1>";
 	}
 
 	public static void main(String[] args) {
